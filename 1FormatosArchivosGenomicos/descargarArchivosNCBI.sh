ac57b36e53eca5b3daa372765c61cf28f01dc9cd
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2025-10-10 15:50:01 -0700

    Create descargarArchivosNCBI.sh

---
+++ 1FormatosArchivosGenomicos/descargarArchivosNCBI.sh
@@ -0,0 +1,12 @@
+codigo para descargar archivos
+
+codigo para calcular fastqc
+
+ejemplos de como limpiar los reads
+
+codigo para hcer mapping y producir sam
+
+codigo para convertir a bam
+
+codigo para generar vcf
+   
