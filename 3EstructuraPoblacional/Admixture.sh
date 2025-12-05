3afd49482397ae342bfa883361544907ed889568
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2025-12-05 00:11:05 -0800

    Update Admixture.sh

--- 3EstructuraPoblacional/Admixture.sh
+++ 3EstructuraPoblacional/Admixture.sh
@@ -5,27 +5,43 @@ module load plink/1.90b624
 module load vcftools
 
 ###############################
-#Apply thinning and compress the result
+# Apply thinning and compress the result
 ###############################
-vcftools --vcf tmp_maf_filtered.recode.vcf --recode --thin 10000 --recode-INFO-all \
-  --stdout | bgzip -c > briceiOutgroupAll_maf2_Thinned10Kb.vcf.gz
+
+vcftools \
+  --gzvcf subsetfinWhale_PASS_VAR_biallelicSNPs.vcf.gz \
+  --thin 10000 \
+  --recode \
+  --recode-INFO-all \
+  --stdout | bgzip -c > subsetfinWhale_PASS_VAR_biallelicSNPs_Thinned10Kb.vcf.gz
+
+# Index
+bcftools index subsetfinWhale_PASS_VAR_biallelicSNPs_Thinned10Kb.vcf.gz
+
 ###############################
 # Prepare data for ADMIXTURE
 ###############################
-# Convert PLINK files to the correct format
-plink --bfile finwhale \
+
+# Convert to PLINK bed files
+plink --vcf subsetfinWhale_PASS_VAR_biallelicSNPs_Thinned10Kb.vcf.gz \
       --make-bed \
       --allow-extra-chr \
-      --out finwhale_admix
+      --out finwhale_admix \
+      --double-id \
+      --keep-allele-order
 
+
+###############################
+# Run ADMIXTURE K=1–10
+###############################
 
 ADMIXTURE=/u/project/klohmuel/mkenfiel/OutgroupSpecies/RicesWhale/admixture/dist/admixture_linux-1.3.0/admixture
-#run admixture for a K of 1-10, using cross-validation, with 10 threads
-for K in 1 2 3 4 5 6 7 8 9 10; 
-do $ADMIXTURE --cv -j10  briceiOutgroupAll_maf2_Thinned10Kb.bed $K | tee log${K}.out;
+
+for K in 1 2 3 4 5 6 7 8 9 10
+do
+    $ADMIXTURE --cv -j10 finwhale_admix.bed $K | tee log${K}.out
 done
 
 ############ AFTER JOB ############
-#Which K iteration is optimal according to ADMIXTURE ?
+# Which K iteration is optimal according to ADMIXTURE?
 grep -h CV log*.out > log.errors.txt
-#grep -h CV subsetLog*.out > subsetLog.errors.txt
