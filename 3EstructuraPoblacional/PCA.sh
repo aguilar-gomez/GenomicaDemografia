1a6df5d167711862c12a361823361c0f3200dbee
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2025-12-06 09:24:54 -0600

    Update PCA.sh

--- 3EstructuraPoblacional/PCA.sh
+++ 3EstructuraPoblacional/PCA.sh
@@ -1,12 +1,15 @@
 ###############################
 # Convert to PLINK and run PCA
 ###############################
-plink --vcf subsetfinWhale_PASS_VAR_biallelicSNPs.vcf.gz \
+plink --vcf Passing_biallelic_var_19-21.vcf.gz \
       --make-bed \
       --out finwhale \
       --allow-extra-chr \
       --double-id \
       --keep-allele-order
+
+#Total genotyping rate is 0.992468.
+#1,325,140 variants and 10 people pass filters and QC.
 
 plink --bfile finwhale \
       --pca \
@@ -14,3 +17,5 @@ plink --bfile finwhale \
       --allow-extra-chr \
       --out finwhale_pca
 
+#1,141,686 variants removed due to minor allele threshold(s)
+#183,454 variants and 10 people pass filters and QC.
